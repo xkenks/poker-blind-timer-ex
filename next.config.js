873378001255d82443f28042b0f4d9f8eebd3bf8
Your Name <your.email@example.com>
2025-03-16 01:11:56 +0900

    Update Next.js and Vercel configurations, fix import paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react']
+  }
 }
 
 module.exports = nextConfig 
